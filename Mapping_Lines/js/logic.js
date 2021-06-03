4ce44e454731cc5333285378e22c9f64dffb0b11
Atreyee <saha@Atreyee.attlocal.net>
2021-06-03 18:24:53 -0500

    Mappinglines

---
+++ Mapping_Lines/js/logic.js
@@ -0,0 +1,48 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+// Create the map object with a center and zoom level.
+//let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the line single line from LA to SFO.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+// Coordinate for adding multiple lines    
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);  
+
+
+
+// We create the tile layer with black color background that will be the background of our map.
+/*let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);*/
+//mapbox://styles/mapbox/satellite-streets-v11
+// We create the tile layer default color that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    //id: 'mapbox/streets-v11',
+    id: 'mapbox/satellite-streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
