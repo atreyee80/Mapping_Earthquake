3fc4acadc63fd405dd1757bef1e3f0c3f405d1a6
Atreyee <saha@Atreyee.attlocal.net>
2021-06-04 18:12:49 -0500

    Added Earthquake past 7days

--- Mapping_GeoJSON_Points/js/logic.js
+++ Mapping_GeoJSON_Points/js/logic.js
@@ -1,7 +1,10 @@
-// Add console.log to check to see if our code is working.
+/*// Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+//let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
+
 
 
 // Add GeoJSON data.
@@ -22,21 +25,8 @@ let sanFranAirport =
         "geometry":{
             "type":"Point",
             "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-// Grabbing our GeoJSON data.
-L.geoJson(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    pointToLayer: function(feature, latlng) {
-      console.log(feature);
-      return L.marker(latlng)
-      .bindPopup("<h2>" + feature.properties.city+","+feature.properties.name +","+feature.properties.country+ "</h2>");
-      //.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>");
-    }
-
-  }).addTo(map);
-
-  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+]};*/
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     //id: 'mapbox/streets-v11',
@@ -45,5 +35,29 @@ L.geoJson(sanFranAirport, {
     zoomOffset: -1,
     accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map.
+// Then we add our 'streets' tile layer to the map.
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquake/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data).addTo(map);
+});
+
+// Grabbing our GeoJSON data.
+/*L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng)
+      .bindPopup("<h2>" + feature.properties.city+","+feature.properties.name +","+feature.properties.country+ "</h2>");
+      //.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>");
+    }
+
+  }).addTo(map);*/
+
+  
---
+++ Mapping_GeoJSON_Polygons/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiYXRyZXllZTgwIiwiYSI6ImNrcDRrd2pyODAzZDYydXIwMWk2aHducGwifQ.MOShofa3xQmLh8yIPpH3Pw";
